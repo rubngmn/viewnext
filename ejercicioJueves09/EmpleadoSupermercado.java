727abdb78ad23891ffe3b76488bdb8d2225b0b09
Rubén <130440722+rubngmn@users.noreply.github.com>
2023-11-10 13:24:30 +0100

    Add files via upload
    1.1

--- ejercicioJueves09/EmpleadoSupermercado.java
+++ ejercicioJueves09/EmpleadoSupermercado.java
@@ -3,13 +3,14 @@ package com.curso.ejercicioJueves09;
 /**
  * @author Ruben
  */
-public class EmpleadoSupermercado {
+public abstract class EmpleadoSupermercado {
 	// Todos los empleados tienen un número de empleado,
 	// un nombre y una ciudad donde trabajan
 	protected int numEmpleado;
 	protected static String nombreEmpleado;
 	protected String ciudad;
-	
+	protected int caja;
+
 	// Constructor con todos los atributos de los empleados
 	public EmpleadoSupermercado(int numEmpleado, String nombreEmpleado, String ciudad) {
 		super();
@@ -29,13 +30,22 @@ public class EmpleadoSupermercado {
 	public String getCiudad() {
 		return ciudad;
 	}
+
 	// Todos los empleados saludan y reponen
 	protected void saludar() {
 		System.out.println(
-				nombreEmpleado + " con número de empleado " + numEmpleado + " ha saludado a un cliente en " + ciudad);
+				this.getNombreEmpleado() + " con número de empleado " + numEmpleado + " ha saludado a un cliente en " + ciudad);
 	}
 
 	protected void reponer() {
 		System.out.println(nombreEmpleado + " está reponiendo");
+	}
+	
+	public void cobrar() {
+		System.out.println(nombreEmpleado + " ha cobrado a un cliente por la caja " + caja);
+	}
+
+	public void validarParking() {
+		System.out.println(nombreEmpleado + " ha validado el parking a un cliente");
 	}
 }
--- ejercicioJueves09/Supermercado.java
+++ ejercicioJueves09/Supermercado.java
@@ -6,26 +6,28 @@ package com.curso.ejercicioJueves09;
 import java.util.ArrayList;
 
 public class Supermercado {
-	
+
 	public static void main(String[] args) {
 		// Creamos un Arraylist y añadimos los empleados
-		ArrayList<EmpleadoSupermercado> empleados = new ArrayList<EmpleadoSupermercado>();
-		empleados.add(new Reponedor(1, "Andrés", "Valencia"));
-		empleados.add(new Cajero(2, "Jorge", "Bilbao", 6));
-		
-		// Este bucle dictamina si el empleado es de tipo Reponedor o Cajero, 
+		Reponedor reponedor1 = new Reponedor(1, "Andrés", "Valencia");
+		Cajero cajero1 = new Cajero(2, "Jorge", "Bilbao", 6);
+		ArrayList<EmpleadoSupermercado> empleados = new ArrayList<>();
+		empleados.add(reponedor1);
+		empleados.add(cajero1);
+
+		// Este bucle dictamina si el empleado es de tipo Reponedor o Cajero,
 		// en base a eso puede realizar unas acciones u otras
+		System.out.println(empleados.size());
 		for (EmpleadoSupermercado i : empleados) {
-			if (i instanceof AtenderCliente) {
-				((AtenderCliente) i).cobrar();
-				((AtenderCliente) i).validarParking();
+			if (i instanceof Cajero) {
+				((Cajero) i).cobrar();
+				((Cajero) i).validarParking();
+			} else  {
 				((EmpleadoSupermercado) i).saludar();
 				((EmpleadoSupermercado) i).reponer();
-			} else {
-				i.saludar();
-				i.reponer();
 			}
-
+				
+			System.out.println(i.getCiudad());
 		}
 
 	}
